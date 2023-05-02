040cc28082428f160369f48eb44fbfca252de136
Manuel Chicco <ManuelC07@users.noreply.github.com>
2023-05-02 12:22:51 -0300

    add: cambios manu2

--- codi.js
+++ codi.js
@@ -16,3 +16,33 @@ function incrementLikes() {
   document.getElementById("numLikes").innerHTML = likes;
 }
 //Parte del codigo donde se verifica el user, el comentario y si todo esta bien lo comenta.
+// Obtener los valores de los inputs
+const username = document.getElementById("username");
+const comment = document.getElementById("comment");
+const btncomentario = document.getElementById("btn-comentario");
+
+btncomentario.addEventListener('click', function(){
+  // Verificar que se haya ingresado un usuario
+  if (!username) {
+    alert("Por favor, ingrese un usuario");
+    return;
+  }
+  
+  // Verificar que se haya ingresado un comentario
+  if (!comment) {
+    alert("Por favor, ingrese un comentario");
+    return;
+  }
+  
+  // Crear un nuevo elemento de comentario
+  var nuevoComentario = document.createElement("div");
+  nuevoComentario.innerHTML = "<strong>" + username + ":</strong> " + comment;
+  
+  // Agregar el nuevo elemento al contenedor de comentarios
+  var contenedorComentarios = document.getElementById("comentarios");
+  contenedorComentarios.appendChild(nuevoComentario);
+  
+  // Limpiar los inputs
+  document.getElementById("username").value = "";
+  document.getElementById("comment").value = "";
+});

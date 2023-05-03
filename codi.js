701a0791da10441a6728f27ec9184a961a9a0326
MDCoronel <coronelm003@gmail.com>
2023-05-03 11:11:58 -0300

    coments

--- codi.js
+++ codi.js
@@ -1,4 +1,4 @@
-//Aca esta la parte del codigo del boton seguir.
+
 const followButton = document.getElementById('follow-button');
 
 followButton.addEventListener('click', function() {
@@ -8,41 +8,29 @@ followButton.addEventListener('click', function() {
     followButton.textContent = 'Seguir';
   }
 });
-//Parte del codigo para el boton de Me gusta y su contador de likes.
+
 var likes = 200;
 
 function incrementLikes() {
   likes++;
   document.getElementById("numLikes").innerHTML = likes;
-}
-//Parte del codigo donde se verifica el user, el comentario y si todo esta bien lo comenta.
-// Obtener los valores de los inputs
-const username = document.getElementById("username");
-const comment = document.getElementById("comment");
-const btncomentario = document.getElementById("btn-comentario");
+};
 
-btncomentario.addEventListener('click', function(){
-  // Verificar que se haya ingresado un usuario
-  if (!username) {
-    alert("Por favor, ingrese un usuario");
-    return;
-  }
-  
-  // Verificar que se haya ingresado un comentario
-  if (!comment) {
-    alert("Por favor, ingrese un comentario");
-    return;
-  }
-  
-  // Crear un nuevo elemento de comentario
-  var nuevoComentario = document.createElement("div");
-  nuevoComentario.innerHTML = "<strong>" + username + ":</strong> " + comment;
-  
-  // Agregar el nuevo elemento al contenedor de comentarios
-  var contenedorComentarios = document.getElementById("comentarios");
-  contenedorComentarios.appendChild(nuevoComentario);
-  
-  // Limpiar los inputs
-  document.getElementById("username").value = "";
-  document.getElementById("comment").value = "";
-});
+function agregarComentario(evento) {
+  evento.preventDefault();
+  //Obtengo los valores ingresados
+  let usuario = document.getElementById('username').value;
+  let textoComentario = document.getElementById('texto-comentario').value;
+  //Agrego el feedback a la página
+  let texto = document.createTextNode(usuario  + ': ' +  textoComentario);
+  let parrafo = document.createElement('p')
+  parrafo.appendChild(texto);
+
+  document.getElementById('comentarios').appendChild(parrafo);
+
+  //Reinicio los valores de los input
+  document.getElementById('texto-comentario').value = '';
+}
+
+document.getElementById('boton-enviar-comentario')
+  .addEventListener('click', agregarComentario)
